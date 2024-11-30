d183271af83362bfdd309a8e601430d752bc1297
action <action@github.com>
2024-11-30 15:05:25 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Wed Nov 13 2024 07:13:54 GMT+0000 (Coordinated Universal Time)
+//Sat Nov 30 2024 15:05:25 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 var ParamsSignLite = function () {
